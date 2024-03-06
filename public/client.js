c9b6dd0edbf2cec80a5f967f4a146c324e77ac11
marotella <michelle.a.rotella@gmail.com>
2024-03-05 20:30:20 -0500

    fixing axios on front end

--- public/client.js
+++ public/client.js
@@ -15,39 +15,6 @@ const ratingInfoSection = document.getElementById("ratingInfo");
 
 
 
-async function fetchHoroscopeData(currentUser) {
-    try {
-        const userSign = currentUser.sign; 
-        console.log(`This is the ${userSign}`);
-        const response = await fetch(`http://localhost:4000/api/horoscope`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            // credentials: 'include' // Send credentials with the request
-        });
-
-        if (!response.ok) {
-            throw new Error(`Failed to fetch horoscope: ${response.statusText}`);
-        }
-        console.log(response)
-        const data = await response.json();
-        const sign = data.prediction.split(',')[0];
-        const horoscope = data.prediction;
-        const number = data.number
-        const color = data.color
-        const mantra = data.mantra
-        const remedy = data.remedy
-        document.getElementById('sign').innerText = sign;
-        document.getElementById('horoscope').innerText = horoscope;
-        document.getElementById('number').innerText = number;
-        document.getElementById('color').innerText = color;
-        document.getElementById('mantra').innerText = mantra;
-        document.getElementById('remedy').innerText = remedy;
-    } catch (error) {
-        console.error(error);
-    }
-}
 
 async function createUser (event) {
     event.preventDefault();
@@ -72,11 +39,11 @@ async function createUser (event) {
 async function loginUser(event) {
     event.preventDefault();
     const email = document.getElementById('login-email').value;
-    // console.log(email)
+    console.log(email)
     let loginData = {
         email: email
     }
-    // console.log(loginData)
+    console.log(loginData)
 
     try {
         const response = await axios.post(`http://localhost:4000/api/login`, loginData, {withCredentials: true})
@@ -84,8 +51,8 @@ async function loginUser(event) {
         currentUser = response.data
         console.log(currentUser)
         if (currentUser) {
-            // await fetchHoroscopeData(currentUser)
-            window.location.href = 'profile.html';
+            // window.location.href = 'profile.html';
+            await fetchHoroscopeData(currentUser)
 
         } else {
             console.error("Current user is undefined")
@@ -95,6 +62,37 @@ async function loginUser(event) {
     }
 
 }
+
+async function fetchHoroscopeData(currentUser) {
+    try {
+        const userSign = currentUser.sign; 
+        console.log(`This is the ${userSign}`);
+        const response = await axios.get(`http://localhost:4000/api/horoscope`, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            withCredentials: true
+        });
+        console.log(response)
+        const data = await response.data;
+        const sign = data.prediction.split(',')[0];
+        const horoscope = data.prediction;
+        const number = data.number
+        const color = data.color
+        const mantra = data.mantra
+        const remedy = data.remedy
+        document.getElementById('sign').innerText = sign;
+        document.getElementById('horoscope').innerText = horoscope;
+        document.getElementById('number').innerText = number;
+        document.getElementById('color').innerText = color;
+        document.getElementById('mantra').innerText = mantra;
+        document.getElementById('remedy').innerText = remedy;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
 getHoroscopeBtn.addEventListener("click", fetchHoroscopeData);
 signUpForm.addEventListener("submit", createUser)
 loginForm.addEventListener("submit", loginUser)
